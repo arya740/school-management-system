2c2b4eae4a2a703829827aa8b7aa93c8663c2b3b
Arya Singh <aryasingh7090@gmail.com>
2026-02-10 22:46:51 +0530

    Day 3 complete: auth, roles, dashboards working

---
+++ backend/accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+
+    path('dashboard/admin/', views.admin_dashboard, name='admin_dashboard'),
+    path('dashboard/teacher/', views.teacher_dashboard, name='teacher_dashboard'),
+    path('dashboard/student/', views.student_dashboard, name='student_dashboard'),
+    path('dashboard/parent/', views.parent_dashboard, name='parent_dashboard'),
+]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username", "").strip()
+        password = request.POST.get("password", "").strip()
+
+        print("USERNAME RECEIVED:", repr(username))
+        print("PASSWORD LENGTH:", len(password))
+
+        user = authenticate(username=username, password=password)
+        print("AUTH RESULT:", user)
+
+        if user is not None:
+            login(request, user)
+
+            if user.role == "ADMIN":
+                return redirect("admin_dashboard")
+            elif user.role == "TEACHER":
+                return redirect("teacher_dashboard")
+            elif user.role == "STUDENT":
+                return redirect("student_dashboard")
+            elif user.role == "PARENT":
+                return redirect("parent_dashboard")
+            else:
+                return render(request, "accounts/login.html", {
+                    "error": "User role not assigned"
+                })
+
+        return render(request, "accounts/login.html", {
+            "error": "Invalid username or password"
+        })
+
+    return render(request, "accounts/login.html")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required
+def admin_dashboard(request):
+    return render(request, 'accounts/admin_dashboard.html')
+
+
+@login_required
+def teacher_dashboard(request):
+    return render(request, 'accounts/teacher_dashboard.html')
+
+
+@login_required
+def student_dashboard(request):
+    return render(request, 'accounts/student_dashboard.html')
+
+
+@login_required
+def parent_dashboard(request):
+    return render(request, 'accounts/parent_dashboard.html')
